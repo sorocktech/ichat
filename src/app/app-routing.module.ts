818a714ad76461301a493986771c99f0374badcd
Nico <sorocktech@outlook.com>
2021-04-17 14:45:28 +0800

    delete files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,70 +25,13 @@ const routes: Routes = [
       import("./pages/send-message/send-message.module").then((m) => m.SendMessagePageModule),
   },
   {
-    path: "warnmap",
-    loadChildren: () =>
-      import("./pages/warnmap/warnmap.module").then((m) => m.WarnmapPageModule),
-  },
-  {
     path: 'login',
     loadChildren: () => import('./pages/logins/login/login.module').then(m => m.LoginPageModule)
   },
 
-  // 免责条款
-  {
-    path: 'clause-state',
-    loadChildren: () => import('./pages/logins/clause-state/clause-state.module').then(m => m.ClauseStatePageModule)
-  },
-
-  ,
-  {
-    path: 'userinfo',
-    loadChildren: () => import('./pages/userinfo/userinfo.module').then(m => m.UserinfoPageModule)
-  },
-  {
-    path: 'userdetail',
-    loadChildren: () => import('./pages/userdetail/userdetail.module').then(m => m.UserdetailPageModule)
-  },
   {
     path: 'setting',
     loadChildren: () => import('./pages/setting/setting.module').then(m => m.SettingPageModule)
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./pages/appversion/appversion.module').then(m => m.AppversionPageModule)
-  },
-  // 群成员
-  {
-    path: 'chatmembers',
-    loadChildren: () => import('./safes/companys/chatmembers/chatmembers.module').then( m => m.ChatmembersPageModule)
-  },
-  {
-    path: 'image-modal',
-    loadChildren: () => import('./pages/image-modal/image-modal.module').then( m => m.ImageModalPageModule)
-  },
-  {
-    path: 'darkmode',
-    loadChildren: () => import('./pages/darkmode/darkmode.module').then( m => m.DarkmodePageModule)
-  },
-  {
-    path: 'qrcode',
-    loadChildren: () => import('./pages/qrcode/qrcode.module').then(m => m.QrcodePageModule)
-  },
-  {
-    path: 'join-group',
-    loadChildren: () => import('./pages/join-group/join-group.module').then(m => m.JoinGroupPageModule)
-  },
-  {
-    path: 'group-qrcode',
-    loadChildren: () => import('./pages/group-qrcode/group-qrcode.module').then(m => m.GroupQrcodePageModule)
-  },
-  {
-    path: 'login-confirm',
-    loadChildren: () => import('./pages/login-confirm/login-confirm.module').then( m => m.LoginConfirmPageModule)
-  },
-  {
-    path: 'account-select',
-    loadChildren: () => import('./pages/account-select/account-select.module').then(m => m.AccountSelectPageModule)
   },
 ];
 @NgModule({
