fe20672b57469db27c2d2ae092db974efbc5ddbb
Nico <sorocktech@outlook.com>
2021-05-12 00:21:29 +0800

    简化路由

--- src/app/api/app.api.ts
+++ src/app/api/app.api.ts
@@ -11,8 +11,6 @@ export class apiList implements OnInit {
   otherurl: any = "";
   regeo: any = "";
   loginList: any = {}; //登录注册模块
-  warnList: any = {}; //一键报警
-  homeList: any = {};
   userList: any = {};
   im: any = {};
   safesList: any = {}; //安全系统
@@ -24,7 +22,6 @@ export class apiList implements OnInit {
     // this.baseurl = "https://crisisresponse.tihal.cn/";
     // this.baseurl = "https://dhdev.tihal.cn:8022/";
     // this.baseurl = "https://testing.tihal.cn:9082/";
-    this.otherurl = "https://yjglptys.tihal.cn:88/";
 
     this.common = {
       doUpload: `${this.baseurl}file/file/upload`, //上传
@@ -42,17 +39,6 @@ export class apiList implements OnInit {
     this.regeo ={
       baidu:'http://api.map.baidu.com/reverse_geocoding/v3'//百度
     }
-    this.warnList = {
-      warnChat: `${this.baseurl}tihal/app-alarm/create`, //一键报警
-      warnStatus: `${this.baseurl}tihal/app-alarm/status-v2`,// 查询报警状态信息
-      isInChina: `${this.baseurl}tihal/app-alarm/china`, //判断经纬度是否在中国
-    };
-    this.homeList = {
-      getWarningList: `${this.baseurl}tihal/app-focus/get-focus-area-all`,
-      getRiskList: `${this.baseurl}tihal/app-focus/text-risk-v4`,
-      getAlarmPhone: `${this.baseurl}tihal/app-phone/alarm-telephone`, //报警电话
-      getBanners: `${this.baseurl}tihal/app-picture/protection-mall`, //首页banner图
-    };
 
     this.userList = {
       refreshToken:`${this.baseurl}tihal/app/refresh-token`,
@@ -64,43 +50,7 @@ export class apiList implements OnInit {
       appTenantUserModify: `${this.baseurl}tihal/app-tenant/app-tenant-user-modify`, //提交修改
       avatarModify: `${this.baseurl}tihal/app-tenant/app-photo-save`, //提交修改
     };
-    this.im ={
-      renameChatRoomName: `${this.baseurl}tihal/im/rename-chat-room`,
-    }
     this.safesList = {
-      // 国别信息
-      getCountryInfoList: `${this.baseurl}tihal/app-country/get-continent-v3`, //国别信息
-      getCountryList: `${this.baseurl}tihal/app-country/get-continent`, //国别信息
-      getCountryAreas: `${this.baseurl}tihal/app-country/get-region`, //国别信息->获取地区信息
-      doCare: `${this.baseurl}tihal/app-focus/focus-area-add`, //用户关注/取消关注
-      cacleCare: `${this.baseurl}tihal/app-focus/focus-area-add`, //取消关注
-      // 自救指南
-      getSelfSaveType: `${this.baseurl}tihal/app-oneself/save-oneself-class`, //自救指南分类
-      getSelfSaveList: `${this.baseurl}tihal/app-oneself/save-oneself`, //自救指南列表
-
-      // 汇率兑换
-      rateExchange: `${this.baseurl}tihal/currency/currency-query`, //汇率列表
-      rateTypeList: `${this.baseurl}tihal/currency/currency-list`, //货币种类列表
-      rateConvert: `${this.baseurl}tihal/currency/currency-exchange`, //货币兑换比例
-      // 应急预案
-      // getEmergenType: `${this.baseurl}tihal/emergency/plan-list`, //应急预案列表
-      plan: `${this.baseurl}tihal/file/files-tree`,
-
-      // 安全周报列表
-      getSafeWeeklyList: `${this.baseurl}tihal/report/weekly-index`,
-
-      // 舆情送报
-      opinionSubmit: `${this.baseurl}tihal/sentiment/sentiment-report`, //舆情报送新建舆情
-      //新冠疫情快速报送单
-      addPlague: `${this.baseurl}/tihal/project/add-plague`,
-      // 疫情监测
-      globalEpidemic: `${this.baseurl}tihal/epidemic/all`, //全球疫情
-      globalDisease: `${this.baseurl}tihal/epidemic/list`, //全球疾病
-      globalNews: `${this.baseurl}tihal/epidemic/news`, //全球最新新闻
-      getCountry: `${this.baseurl}tihal/project/get-project-for-country`, //国别
-      getOrganizationV2: `${this.baseurl}tihal/organization/get-organization-v2`, //子企业名称
-      getNewsDtail: `${this.baseurl}tihal/epidemic/news-deatil`, //新闻详情
-
       // 企业微信
       getContactsList: `${this.baseurl}tihal/im/get-i-m-contacts1`, //通讯录
 
@@ -124,31 +74,6 @@ export class apiList implements OnInit {
       // 聊天列表
       getChatList: `${this.baseurl}/tihal/im/message-list`,
       updateChatList: `${this.baseurl}/tihal/im/message-list`,
-
-      getTypeAssistanceGlobal: `${this.baseurl}tihal/app-global/assistance-global`, //境外救援
-      getTypeInsurance: `${this.baseurl}tihal/app-insurance/overseas-insurance`, //保险保障
-      getTypeClaims: `${this.baseurl}tihal/app-insurance/insurance-claim`, //保险理赔
-      getTypeProtect: `${this.baseurl}tihal/app-mall/protection-mall`, //防护商城
-
-      // 事件上报
-      eventsSubmit: `${this.baseurl}tihal/emergency/police-add`, //事件上报
-      getEventCountry: `${this.baseurl}tihal/area/get-country`, //事件上报--事发国家选择
-
-      eventTypeList: `${this.baseurl}tihal/self-type/get-self-type`, //事件类型
-      companyList: `${this.baseurl}tihal/organization/get-organization`, //所属组织
-
-      // 防控日报
-      preventAdd: `${this.baseurl}tihal/epidemic-daily/create`,//防控日报新增
-      preventLogList: `${this.baseurl}tihal/epidemic-daily/list`,//防控日报列表
-      projectlist: `${this.baseurl}tihal/epidemic-daily/projects`,//境外项目列表
-      companyslist: `${this.baseurl}tihal/epidemic-daily/company`,//权属单位
-
-      // 信息统计
-      staticsInfoList: `${this.baseurl}tihal/project/get-projects`,//信息统计列表
-      allselectsList: `${this.baseurl}tihal/project/get-code`,//所有下拉-区域行业等
-      searchInfoDetail: `${this.baseurl}tihal/project/last-project`,//项目信息详情
-      editProjectInfo: `${this.baseurl}tihal/project/add-project`,//修改项目信息
-
     };
   }
   ngOnInit() { }
