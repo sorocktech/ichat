17fe4d07baf43ddce9217b9d45b2de327d5195ce
nico <sorocktech@outlook.com>
2021-06-17 01:25:50 +0800

    sign up page

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NavController,  ToastController } from '@ionic/angular';
 import { apiList } from 'src/app/api/app.api';
 import { HttpService } from 'src/app/sevices/http.service';
+import { FormGroup,Validators, FormControl, FormBuilder } from '@angular/forms';
+import { MustMatch } from './must-match.validator';
+
 
 @Component({
   selector: 'app-signup',
@@ -9,22 +12,28 @@ import { HttpService } from 'src/app/sevices/http.service';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-  public username: string
-  public password: string
-  public password_repeat: string
-
+    signForm:FormGroup
   constructor(
     public http: HttpService,
     public api: apiList,
     public nav: NavController,
+    public formBuilder: FormBuilder,
     public toast:ToastController
-  ) { }
+  ) { 
+    this.signForm= this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    }, {validator: MustMatch('password', 'confirmPassword')
+  })
+
+  }
 
   ngOnInit() {
   }
 
 
-  async message(message:string) {
+  async showMessage(message:string) {
     const toast = await this.toast.create({
       message: message,
       position: 'bottom',
@@ -34,25 +43,19 @@ export class SignupPage implements OnInit {
   }
   // 登录
   async signup() {
-    if(this.password != this.password_repeat){
-      this.message('两次输入密码不一致')
-      return false 
+    if(!this.signForm.valid){
+      return true
     }
-    if(!this.username){
-      this.message('用户名不能为空')
-      return false
-    }
-
-    let req = {
-      username:this.username,
-      password:this.password,
-    };
-
-    this.http.post(this.api.loginList.signup, req, async(res) => {
-          console.log(res)
-         this.message('密码或账号错误');
-         return true
-        await this.nav.navigateForward(["/home"]);
+    this.http.post(this.api.loginList.signup, this.signForm.value, async (res) => {
+      if (res.error) {
+        console.log(res)
+        return this.showMessage(res.error.data.message)
+      }
+      this.showMessage('注册成功')
+      setTimeout(async()=>{
+            await this.nav.navigateForward(["/login"])
+      },1000)
+      return true 
     })
 
   }
