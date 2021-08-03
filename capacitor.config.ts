48d80569a96f25e8e38f0f056b8c8b2af6a391ce
chao <sorocktech@outlook.com>
2021-08-03 18:04:10 +0800

    change

---
+++ capacitor.config.ts
@@ -0,0 +1,25 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'collect-app',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  cordova: {
+    preferences: {
+      ShowSplashScreenSpinner: 'false',
+      AutoHideSplashScreen: 'true',
+      FadeSplashScreen: 'true',
+      SplashScreenBackgroundColor: 'false',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'true',
+      SplashScreen: 'splash',
+      AndroidXEnabled: 'true',
+      SplashScreenDelay: '300',
+      'auto-hide-splash-screen': 'true'
+    }
+  }
+};
+
+export default config;
