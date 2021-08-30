2738de6470c9d0d9fad94d57247f7c1f23c0a789
nico <0224wc@gmail.com>
2021-08-31 01:21:30 +0800

    统一查询

--- src/app/pages/linkmanlist/linkmanlist.page.ts
+++ src/app/pages/linkmanlist/linkmanlist.page.ts
@@ -7,67 +7,65 @@ import { apiList } from "../../api/app.api"; // 引入
 import { DataService } from "../../sevices/data.service";
 import { contactsItem, TypeContacts } from 'src/app/interfaces/chat';
 import PouchDB from 'node_modules/pouchdb';
+import { ChatWithDb } from 'src/app/providers/chatWithDb';
 
 @Component({
-  selector: 'app-linkmanlist',
-  templateUrl: './linkmanlist.page.html',
-  styleUrls: ['./linkmanlist.page.scss'],
-  encapsulation:ViewEncapsulation.None,
-  styles:[
+  selector: "app-linkmanlist",
+  templateUrl: "./linkmanlist.page.html",
+  styleUrls: ["./linkmanlist.page.scss"],
+  encapsulation: ViewEncapsulation.None,
+  styles: [
     `
-    .hightText span{
-      color:rgb(50, 89, 199);
-    }
-    `
-  ]
+      .hightText span {
+        color: rgb(50, 89, 199);
+      }
+    `,
+  ],
 })
 export class LinkmanlistPage extends BaseUI implements OnInit {
   pouchdb: any;
   public linkmanList: Array<contactsItem> = [];
   public orgid: string = "";
-  public search: string = "";//搜索
-  public checktype: string = "";//区分是建群
-  constructor(public nav: NavController,
+  public search: string = ""; //搜索
+  public checktype: string = ""; //区分是建群
+  constructor(
+    public nav: NavController,
     public router: Router,
     public http: HttpService,
     public api: apiList,
+    public chatDb: ChatWithDb,
     public loadingCtrl: LoadingController,
     public activeRoute: ActivatedRoute,
-    public dataService: DataService,
-
-    ) {
+    public dataService: DataService
+  ) {
     super();
     this.activeRoute.queryParams.subscribe((params: Params) => {
       this.orgid = params.orgid;
       this.checktype = params.checktype;
     });
 
-    this.pouchdb = this.dataService.db
+    this.pouchdb = this.dataService.db;
   }
 
   ngOnInit() {
     this.getList();
   }
-  
+
   /**
    * 获取联系人列表
    */
   async getList() {
-    try {
-      let res = await this.pouchdb.find({
-        selector: { data_type: TypeContacts },
-      });
-      this.linkmanList = res.docs
-    } catch (err) {
-      console.log(err);
-    }
+    this.linkmanList = await this.chatDb.queryContacts();
   }
 
   goChange(title, inx) {
-    if (title.id == '0') {
+    if (title.id == "0") {
       super.backLastPage(this.nav);
     }
-    this.dataService.linkmanList.splice(inx + 1, this.dataService.linkmanList.length - 1);
+    this.dataService.linkmanList.splice(
+      inx + 1,
+      this.dataService.linkmanList.length - 1
+    );
     this.orgid = title.id;
     this.getList();
   }
@@ -76,10 +74,14 @@ export class LinkmanlistPage extends BaseUI implements OnInit {
   goBack() {
     if (this.dataService.linkmanList.length == 2) {
       super.backLastPage(this.nav);
-    }
-    else {
-      this.dataService.linkmanList.splice(this.dataService.linkmanList.length - 1, this.dataService.linkmanList.length - 1);
-      this.orgid = this.dataService.linkmanList[this.dataService.linkmanList.length - 2].id;
+    } else {
+      this.dataService.linkmanList.splice(
+        this.dataService.linkmanList.length - 1,
+        this.dataService.linkmanList.length - 1
+      );
+      this.orgid = this.dataService.linkmanList[
+        this.dataService.linkmanList.length - 2
+      ].id;
       this.getList();
     }
   }
