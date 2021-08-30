fcc3d53bee2478b8ed0de38003fed075ca89fba4
nico <0224wc@gmail.com>
2021-08-31 00:25:51 +0800

    approve friends

--- src/app/components/notice/notice.page.ts
+++ src/app/components/notice/notice.page.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 import { ChatItem, ChatMessageItem, contactsReq, MessageItem } from 'src/app/interfaces/chat';
+import { NavController } from '@ionic/angular';
+import { DataService } from 'src/app/sevices/data.service';
 
 @Component({
   selector: "app-notice",
@@ -7,16 +9,20 @@ import { ChatItem, ChatMessageItem, contactsReq, MessageItem } from 'src/app/int
   styleUrls: ["./notice.page.scss"],
 })
 export class NoticePage implements OnInit {
-  @Input() ChatMessageItem: ChatMessageItem = undefined
-  public NoticeItem:contactsReq
-  constructor() {}
-i
+  @Input() ChatMessageItem: ChatMessageItem = undefined;
+  public NoticeItem: contactsReq;
+  constructor(
+    public data: DataService,
+    public nav: NavController
+    ) {}
   ngOnInit() {
     console.log(this.ChatMessageItem);
-    this.NoticeItem = JSON.parse(this.ChatMessageItem.ChatItem.message.text) || null
+    this.NoticeItem =
+      JSON.parse(this.ChatMessageItem.ChatItem.message.text) || null;
   }
   viewContactsReq() {
-    console.log('work')
-
+    console.log("work");
+    this.data.currentSearchedUser = this.NoticeItem.param;
+    this.nav.navigateForward("/contact/search");
   }
 }
