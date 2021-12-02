9c4dffe0355014993a14507b43ace91f74693ea6
chao <sorocktech@outlook.com>
2021-12-02 15:19:00 +0800

    创建笔记

---
+++ src/app/pages/create-note/create-note.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-note',
+  templateUrl: './create-note.page.html',
+  styleUrls: ['./create-note.page.scss'],
+})
+export class CreateNotePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
